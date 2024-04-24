4c59293daed61c087cfd2d719cd45cfcd20aa9b1
Krish Poptani <poptanikrish1@gmail.com>
2024-04-24 12:09:50 +0530

    Header

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,9 @@
-import { Button } from "@/components/ui/button";
+ 
 
 export default function Home() {
   return (
     <main className="">
       <h1>Linked In Clone</h1>
-      <Button>Click me</Button>
     </main>
   );
 }
