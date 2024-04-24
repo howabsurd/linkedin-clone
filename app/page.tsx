47e30ab7e5b3fcf3b66c92db2c23f7190baabacd
Krish Poptani <poptanikrish1@gmail.com>
2024-04-24 16:50:34 +0530

    Sidebar Profile

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,25 @@
- 
+ import UserInformation from "@/components/UserInformation";
+import { SignedIn } from "@clerk/nextjs";
 
 export default function Home() {
   return (
-    <main className="">
-      <h1>Linked In Clone</h1>
-    </main>
+    <div className="grid grid-cols-8 mt-5 sm:px-5">
+    <section className="hidden md:inline md:col-span-2">
+      <UserInformation />
+    </section>
+
+    
+
+    <section className="col-span-full md:col-span-6 xl:col-span-4 xl:max-w-xl mx-auto w-full">
+      {/* <SignedIn>
+        <PostForm />
+      </SignedIn>
+      <PostFeed posts={posts} /> */}
+    </section>
+
+    <section className="hidden xl:inline justify-center col-span-2">
+      {/* <Widget /> */}
+    </section>
+  </div>
   );
 }
